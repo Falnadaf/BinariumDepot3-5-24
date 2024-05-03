31ab2d44a9d15a7f478bf6797055a21c2f2812b5
Falnadaf <101874631+Falnadaf@users.noreply.github.com>
2024-05-03 10:34:26 +0200

    Update index.js

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL = 'https://storehouse.onrender.com'
+export const BACKEND_URL = 'https://binariumdepot3-5-24.onrender.com'
 // export const HOST_URL = 'https://storehouse.onrender.com'
 // export const BACKEND_URL = 'http://localhost:8080'
 export const HOST_URL = 'https://gaming-console.netlify.app'
